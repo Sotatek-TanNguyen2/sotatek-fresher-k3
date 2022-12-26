2e802789dd4180b331ec04a153f66832349b4ead
Tan <ngoctana1k2014@gmail.com>
2022-12-26 14:53:06 +0700

    setup backend

---
+++ nestjs-social-be/src/models/repositories/user.repository.ts
@@ -0,0 +1,18 @@
+import { UserEntity } from 'src/models/entities/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+
+@EntityRepository(UserEntity)
+export class UserRepository extends Repository<UserEntity> {
+  async findUserByAccountId(accountId: number): Promise<UserEntity> {
+    const user = await this.createQueryBuilder('users')
+      .select('*')
+      .innerJoin('accounts', 'accounts', 'accounts.ownerId = users.id')
+      .where('accounts.id = :accountId', { accountId })
+      .execute();
+    if (user[0]) {
+      return user[0];
+    } else return null;
+  }
+}
+
+// Repository.find(UserEntity, {id: 1})
---
+++ nestjs-social-be/src/shares/numberFormatter.ts
@@ -0,0 +1,8 @@
+export const formatNumber = (number: string | undefined, precision: number, zeroValue: string): string => {
+  if (number === undefined || number === null || number === '' || Number(number) == NaN) {
+    return zeroValue;
+  }
+  return Number(number)
+    .toFixed(precision)
+    .replace(/\d(?=(\d{3})+\.)/g, '$&,');
+};

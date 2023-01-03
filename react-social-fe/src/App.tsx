8938fd2ae4c6a0f576a4600927c19c094aef5c56
Tan <ngoctana1k2014@gmail.com>
2023-01-03 11:36:27 +0700

    login signup UI

--- react-social-fe/src/App.tsx
+++ react-social-fe/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Auth/Login';
+import Home from './pages/Home/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Social App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ react-social-fe/src/pages/Auth/Login.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Box, IconButton, InputAdornment, Typography } from '@mui/material';
+import { CustomButton, Input, Label, SubmitBtn } from './styled';
+import bgLogin from '../../assets/imgs/bg_login.svg';
+
+const Login: React.FC = () => {
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{ height: '90%', position: 'relative' }}>
+        <img height="100%" src={bgLogin} alt="login background" />
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '20%',
+            right: '7%',
+            width: '500px',
+            bgcolor: '#fff',
+            borderRadius: '30px',
+            boxShadow: '0px 8px 50px rgba(150, 140, 169, 0.1)',
+            padding: '60px',
+          }}
+        >
+          <form action="">
+            <Typography fontSize={39} fontWeight={600}>
+              {isSignUp ? 'Sign Up' : 'Sign In'}
+            </Typography>
+            <Typography sx={{ marginBottom: '20px' }}>
+              {isSignUp ? 'Have an account?' : "Don't have an account?"}
+              <CustomButton onClick={() => setIsSignUp((prev) => !prev)}>
+                {isSignUp ? 'Sign In' : 'Sign Up'}
+              </CustomButton>
+            </Typography>
+
+            <Label>Email Address</Label>
+            <Input fullWidth />
+            <Label sx={{ marginTop: '20px' }}>Password</Label>
+            <Input
+              fullWidth
+              type={showPassword ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            {isSignUp && (
+              <>
+                <Label sx={{ marginTop: '20px' }}>Confirm Password</Label>
+                <Input
+                  fullWidth
+                  type={showPassword ? 'text' : 'password'}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </>
+            )}
+            <SubmitBtn variant="contained">
+              {isSignUp ? 'Sign Up' : 'Sign In'}
+            </SubmitBtn>
+            <CustomButton
+              href="#"
+              sx={{
+                marginTop: '20px',
+              }}
+            >
+              Forgot your password?
+            </CustomButton>
+          </form>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;

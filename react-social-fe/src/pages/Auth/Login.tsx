49c2bb19b34458e210bb5536a9539178bd7f11ad
Tan <ngoctana1k2014@gmail.com>
2023-01-03 12:58:05 +0700

    login signup UI

--- react-social-fe/src/pages/Auth/Login.tsx
+++ react-social-fe/src/pages/Auth/Login.tsx
@@ -31,113 +31,115 @@ const Login: React.FC = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundImage: `url(${bgLogin})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        position: 'relative',
       }}
     >
-      <Box sx={{ height: '90%', position: 'relative' }}>
-        <img height="100%" src={bgLogin} alt="login background" />
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '20%',
-            right: '7%',
-            width: '500px',
-            bgcolor: '#fff',
-            borderRadius: '30px',
-            boxShadow: '0px 8px 50px rgba(150, 140, 169, 0.1)',
-            padding: '60px',
-          }}
-        >
-          <form action="">
-            <Typography fontSize={39} fontWeight={600}>
-              {isSignUp ? 'Sign Up' : 'Sign In'}
-            </Typography>
-            <Typography sx={{ marginBottom: '20px' }}>
-              {isSignUp ? 'Have an account?' : "Don't have an account?"}
-              <CustomButton onClick={() => setIsSignUp((prev) => !prev)}>
-                {isSignUp ? 'Sign In' : 'Sign Up'}
-              </CustomButton>
-            </Typography>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          right: '10%',
+          transform: 'translateY(-50%)',
+          maxWidth: '500px',
+          bgcolor: '#fff',
+          borderRadius: '30px',
+          boxShadow: '0px 8px 50px rgba(150, 140, 169, 0.1)',
+          padding: '60px',
+        }}
+      >
+        <form action="">
+          <Typography fontSize={39} fontWeight={600}>
+            {isSignUp ? 'Sign Up' : 'Sign In'}
+          </Typography>
+          <Typography sx={{ marginBottom: '20px' }}>
+            {isSignUp ? 'Have an account?' : "Don't have an account?"}
+            <CustomButton onClick={() => setIsSignUp((prev) => !prev)}>
+              {isSignUp ? 'Sign In' : 'Sign Up'}
+            </CustomButton>
+          </Typography>
 
-            <Label>Email Address</Label>
-            <Input fullWidth />
-            <Label sx={{ marginTop: '20px' }}>Password</Label>
-            <Input
-              fullWidth
-              type={showPassword ? 'text' : 'password'}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            {isSignUp && (
-              <>
-                <Label sx={{ marginTop: '20px' }}>Confirm Password</Label>
-                <Input
-                  fullWidth
-                  type={showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                        >
-                          {showPassword ? <Visibility /> : <VisibilityOff />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </>
-            )}
-            {isSignUp && (
-              <Stack ml="-9px" direction="row" alignItems="center">
-                <Checkbox
-                  size="small"
-                  sx={{
-                    color: '#9854df',
-                    '&.Mui-checked': {
-                      color: '#b772ff',
-                    },
-                  }}
-                />
-                <Typography sx={{ display: 'inline-block', fontSize: '13px' }}>
-                  I accept the{' '}
-                  <CustomButton href="#" sx={{ display: 'inline-block' }}>
-                    Terms of Conditions.
-                  </CustomButton>
-                </Typography>
-              </Stack>
-            )}
-
-            <SubmitBtn
-              loading={loading}
-              loadingIndicator="Loading..."
-              type="submit"
-              variant="contained"
-            >
-              {isSignUp ? 'Sign Up' : 'Sign In'}
-            </SubmitBtn>
-            {!isSignUp && (
-              <CustomButton
-                href="#"
-                sx={{
-                  marginTop: '20px',
+          <Label>Email Address</Label>
+          <Input fullWidth />
+          <Label sx={{ marginTop: '20px' }}>Password</Label>
+          <Input
+            fullWidth
+            type={showPassword ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          {isSignUp && (
+            <>
+              <Label sx={{ marginTop: '20px' }}>Confirm Password</Label>
+              <Input
+                fullWidth
+                type={showPassword ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
                 }}
-              >
-                Forgot your password?
-              </CustomButton>
-            )}
-          </form>
-        </Box>
+              />
+            </>
+          )}
+          {isSignUp && (
+            <Stack ml="-9px" direction="row" alignItems="center">
+              <Checkbox
+                size="small"
+                sx={{
+                  color: '#9854df',
+                  '&.Mui-checked': {
+                    color: '#b772ff',
+                  },
+                }}
+              />
+              <Typography sx={{ display: 'inline-block', fontSize: '13px' }}>
+                I accept the{' '}
+                <CustomButton href="#" sx={{ display: 'inline-block' }}>
+                  Terms & Conditions.
+                </CustomButton>
+              </Typography>
+            </Stack>
+          )}
+
+          <SubmitBtn
+            loading={loading}
+            loadingIndicator="Loading..."
+            type="submit"
+            variant="contained"
+          >
+            {isSignUp ? 'Sign Up' : 'Sign In'}
+          </SubmitBtn>
+          {!isSignUp && (
+            <CustomButton
+              href="#"
+              sx={{
+                marginTop: '20px',
+              }}
+            >
+              Forgot your password?
+            </CustomButton>
+          )}
+        </form>
       </Box>
     </Box>
   );

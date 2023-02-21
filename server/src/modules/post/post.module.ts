c59ce8b5ba14022cfa03fe55c6cff1d530b8b822
Tan <ngoctana1k2014@gmail.com>
2023-02-21 10:57:54 +0700

    add redis

--- server/src/modules/post/post.module.ts
+++ server/src/modules/post/post.module.ts
@@ -1,18 +1,32 @@
-import { UploadModule } from './../upload/upload.module';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as redisStore from 'cache-manager-redis-store';
+import { PostEntity } from './../../models/entities/post.entity';
+import { PostRepository } from './../../models/repositories/post.repository';
 import { CommentModule } from './../comment/comment.module';
 import { PostMediaModule } from './../post-media/post-media.module';
-import { PostEntity } from './../../models/entities/post.entity';
-import { Module, CacheModule } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostRepository } from './../../models/repositories/post.repository';
+import { UploadModule } from './../upload/upload.module';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([PostEntity]),
+    // CacheModule.registerAsync({
+    //   useFactory: (configService: ConfigService) => ({
+    //     store: redisStore,
+    //     host: configService.get<string>('REDIS_HOST'),
+    //     port: configService.get<number>('REDIS_PORT'),
+    //     ttl: configService.get<number>('REDIS_TTL'),
+    //   }),
+    //   inject: [ConfigService],
+    // }),
     CacheModule.register({
-      ttl: 60,
+      store: redisStore,
+      host: '0.0.0.0',
+      port: 16379,
+      ttl: 3600,
     }),
     PostMediaModule,
     CommentModule,
